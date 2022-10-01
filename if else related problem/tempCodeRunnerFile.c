53487ce220a74f967622730f5b6a292fb861f10a
Dween Mohammad <113244284+dweenmd@users.noreply.github.com>
2022-10-02 02:29:34 +0600

    change rename

--- if else related problem/tempCodeRunnerFile.c
+++ if else related problem/tempCodeRunnerFile.c
@@ -1,15 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-    int a,b,c;
-    printf("Enter three slids:\n");
-    scanf("%d%d%d", &a,&b,&c);
-    if((a+b>c)&&(a+c>b)&&(b+c>a)){
-        printf("The triangle is valid");
+    float n;
+     printf("Enter the number:");
+        scanf("%f",&n);
+    if (n>0)
+    {
+        printf("The number is positive\n");
     }
-    else {
-        printf("The triangle is not valid");
-
+    else if (n<0)
+    {
+       printf("The number is negative\n");
     }
+    else if (n==0)
+    {
+        printf("the number is zero!");
+    }
+    return 0;
 
-return 0;
 }
