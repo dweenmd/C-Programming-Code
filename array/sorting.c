2ae2ec310807fe76150a5566d9e7ddc3f3e5246f
Dween Mohammad <113244284+dweenmd@users.noreply.github.com>
2022-11-05 03:15:13 +0600

    n

--- array/sorting.c
+++ array/sorting.c
@@ -8,16 +8,16 @@ int main(){
     for(int i=0;i<size-1;i++){// Outer loop for steps
        for(int j=0;j<size-i-1;j++){ //Inner loop for comparison and swap
         if(arr[j]>arr[j+1]){
-            int temp= arr[j];
+            int temp= arr[j]; // swap 
             arr[j]=arr[j+1];
             arr[j+1]=temp;
         }
        }
     }
 
-   // for(int i=0; i<size; i++){
-    //    printf("%d ", arr[i]);
-    // }
+//    for(int i=0; i<size; i++){
+//        printf("%d ", arr[i]);
+//     }
     printf("%d ", arr[size-2]);
 
 
