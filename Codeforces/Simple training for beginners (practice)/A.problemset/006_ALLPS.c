4ea6d09bde5ca21714fb7afbbb42999c1665f5eb
dweenmd <dween.md01@gmail.com>
2023-02-17 22:36:12 +0600

    codeforces

--- Codeforces/Simple training for beginners (practice)/A.problemset/006_ALLPS.c
+++ Codeforces/Simple training for beginners (practice)/A.problemset/006_ALLPS.c
@@ -1,10 +1,33 @@
-#include<stdio.h>
-#include<string.h>
-void main()
+// the solve of llps in codeforce problem
+#include <stdio.h>
+#include <string.h>
+int main()
 {
-
-char str[11];
-gets(str);
-
-puts(str);
+    char dween[20];
+    scanf("%s", dween);
+    int size = strlen(dween);
+    // printf("%d\n", size);
+    //  puts(dween);
+    for (int i = 0; i < (size); i++)
+    {
+        for (int j = 0; j < (size - 1); j++)
+        {
+            if (dween[j] > dween[j + 1])
+            {
+                char temp = dween[j];
+                dween[j] = dween[j + 1];
+                dween[j + 1] = temp;
+            }
+        }
+    }
+    // puts(dween);
+    int max = dween[size - 1];
+    for (int i = 0; i < size; i++)
+    {
+        if (dween[i] == max)
+        {
+            printf("%c", max);
+        }
+    }
+    return 0;
 }
