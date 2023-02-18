cf2ffe40672d77334e4d2306d6daf9b868c71245
dweenmd <dween.md01@gmail.com>
2023-02-18 12:37:23 +0600

    string_sort

---
+++ sorting/sorting_string.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char dween[20];
+    scanf("%s", dween);
+    int size = strlen(dween);
+    // printf("%d\n", size);
+    //  puts(dween);
+    for (int i = 0; i < (size); i++)
+    {
+        for (int j = 0; j < (size - 1); j++)
+        {
+            if (dween[j] > dween[j + 1])
+            {
+                char temp = dween[j];
+                dween[j] = dween[j + 1];
+                dween[j + 1] = temp;
+            }
+        }
+    }
+    puts(dween);
+
+    return 0;
+}
