c979494a47a0c70341443802313ccdc2d14eb586
dweenmd <dween.md01@gmail.com>
2023-03-05 00:23:21 +0600

    52-4

--- 52-programming Problems by subin/04divisor.c
+++ 52-programming Problems by subin/04divisor.c
@@ -1,7 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    
+    int n, a;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &a);
+        printf("Case %d:", i);
+        for (int j = 1; j <= a; j++)
+        {
+            if (a % j == 0)
+            {
+                printf(" %d", j);
+            }
+        }
+        printf("\n");
+    }
 }
