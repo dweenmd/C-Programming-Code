b472a7ab60d592feee9f5cb860012e978d16694b
Dween MOhammad <dween.md01@gmail.com>
2023-03-14 18:32:25 +0600

    m

---
+++ 52-programming Problems by subin/07numbercountingpraactice2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+
+    while (a--)
+    {
+        int count =0;
+        char str[100000];
+        printf("input\n");
+
+        getchar();
+        gets(str);
+        puts(str);
+        int n=strlen(str);
+        printf("len: %d\n",n);
+        for (int i=0;i<n;i++){
+
+        }
+    }
+
+}
