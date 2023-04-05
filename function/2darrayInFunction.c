a099d7c73610b14235176cea92e518a56b9cd879
Dween MOhammad <dween.md01@gmail.com>
2023-04-05 17:05:43 +0600

    2d array in function

--- function/2darrayInFunction.c
+++ function/2darrayInFunction.c
@@ -22,8 +22,7 @@ void printcell(int a[][4], int n)
     {
         for (int j = 1; j <= n; j++)
         {
-            if (a[i][j] == -1)
-                printf(" ");
+                printf("%d",a[i][j]);
             if (j < n)
                 printf("\t|\t");
         }
